00205fb433049ef7c007d26b4f4c5cf7677f0d87
rohitkerketta06 <rohitkerketta484@gmail.com>
2026-06-06 06:33:11 +0530

    music playlist

--- Music.java
+++ Music.java
@@ -6,10 +6,10 @@ class Song{
         this.title = title;
         this.artist = artist;
     }
-    String getTitle(){
+    public String getTitle(){
         return title;
     }
-    String getArtist(){
+    public String getArtist(){
         return artist;
     }
 }
@@ -20,12 +20,39 @@ class Playlist{
         this.songs = new Song[size];
         this.count = 0;
     }
-    void addsong(Song s){
-        
+    public void addsong(Song s){
+        if (count < songs.length){
+            songs[count] = s;
+            count++;
+        }
+    }
+    public void shuffle(){
+        Random rand = new Random();
+        for (int i= count-1; i>0; i--){
+            int j = rand.nextInt(i+1);
+            Song temp = songs[i];
+            songs[i] = songs[j];
+            songs[j] = temp;
+        }
+    }
+    public void display(){
+        for (Song s : songs){
+            if (s!=null){
+                System.out.println(s.getTitle() + " - " + s.getArtist());
+            }
+        }
     }
 }
 public class Music{
     public static void main(String[] args){
         Scanner sc = new Scanner (System.in);
+        Playlist myPlaylist = new Playlist(5);
+        myPlaylist.addsong(new Song("Believer", "Imagine Dragons"));
+        myPlaylist.addsong(new Song("Shape of You", "Ed Sheeran"));
+        System.out.println("Original:");
+        myPlaylist.display();
+        myPlaylist.shuffle();
+        System.out.println("Shuffled:");
+        myPlaylist.display();
     }
 }
